8087e11ebae6d1398bdff89a58020b4f1dd73e00
Simi <simike.varga@gmail.com>
2024-02-13 17:12:27 +0100

    asdasdadadsdad

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,34 @@
-import { Actions } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "../../.svelte-kit/types/src/routes/$types";
+import { prisma } from '../lib/server/prisma';
+import { fail } from "@sveltejs/kit"
 
+
+export const load: PageServerLoad = async () => {
+	return {
+		articles: await prisma.article.findMany(),
+	}
+}
 export const actions: Actions = {
-    
-};
+	createArticle: async ({ request }) => {
+		const { title, content } = Object.fromEntries(await request.formData()) as {
+			title: string
+			content: string
+		}
+
+		try {
+			await prisma.article.create({
+				data: {
+					title,
+					content,
+				},
+			})
+		} catch (err) {
+			console.error(err)
+			return fail(500, { message: "Could not create the article." })
+		}
+
+		return {
+			status: 201,
+		}
+	},
+}
